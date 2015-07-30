6870a4d5464f078f1f91aab85a0b2f9f8a685aa7
marcosf63 <marcosf63@gmail.com>
2015-07-29 21:34:00 -0300

    Corrige alguns erros

--- notes.bat
+++ notes.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 REM Ler o Notes ini  e vefirica a versao do notes
 
-REM mapea o drive m para a pasta VersaoNOtes no suporte da DRFSOB e manda a saida para null.txt
+REM Mapeia o drive m para a pasta VersaoNotes no suporte da localidade e manda a saida para null.txt
 net use m: \\10.53.52.6\suporte$\VersaoNotes > null.txt
 
-REM Define o caminho do notes ini na máquina do usuário
+REM Define o caminho do notes.ini na máquina do usuário
 SET caminho=%USERPROFILE%\AppData\Local\Lotus\Notes\Data\notes.ini
 
-REM Procura e versao do notes do notes.ini e grava no notes.txt versao e nome da maquina
+REM Procura e versao no notes do notes.ini e grava no notes.txt versao e nome da maquina
 findstr "^FaultRecovery_Build=.*" %caminho% >> m:\notes.txt && echo %ComputerName% >> m:\notes.txt
 
-REM delta null txt.
+REM deleta null txt.
 del null.txt
